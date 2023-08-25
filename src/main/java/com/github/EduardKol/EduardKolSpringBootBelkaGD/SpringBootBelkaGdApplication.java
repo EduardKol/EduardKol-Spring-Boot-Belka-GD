7d0dc23869388aec6a4ae7f66365f3e6008b17e4
EduardKol <eduardkolesnikov@mail.ru>
2023-08-25 23:01:42 +0300

    #first commit

---
+++ src/main/java/com/github/EduardKol/EduardKolSpringBootBelkaGD/SpringBootBelkaGdApplication.java
@@ -0,0 +1,13 @@
+package com.github.EduardKol.EduardKolSpringBootBelkaGD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBelkaGdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBelkaGdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/EduardKol/EduardKolSpringBootBelkaGD/SpringBootBelkaGdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.EduardKol.EduardKolSpringBootBelkaGD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBelkaGdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
